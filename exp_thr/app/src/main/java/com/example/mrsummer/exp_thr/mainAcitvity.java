8e592356afea285a0d659b8bd72f083833705f05
MrSummer <37925293+Mr0Summer@users.noreply.github.com>
2018-05-06 10:29:45 +0800

    3

--- exp_thr/app/src/main/java/com/example/mrsummer/exp_thr/mainAcitvity.java
+++ exp_thr/app/src/main/java/com/example/mrsummer/exp_thr/mainAcitvity.java
@@ -1,5 +1,6 @@
 package com.example.mrsummer.exp_thr;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,33 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class mainAcitvity extends AppCompatActivity {
-
+public class mainAcitvity extends AppCompatActivity
+        implements View.OnClickListener
+{
+    //Activity自身作为事件监听器
+    Button bn4;
+    EditText show;
+    //匿名内部类
+    Button bn3;
     Button out;
-    EditText nm_txt;
-    Button nm;
-
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        nm_txt = (EditText) findViewById(R.id.nm_txt);
-        nm = (Button) findViewById(R.id.nm);
-
-        nm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nm_txt.setText("匿名内部类实现");
-            }
-        });
-        //匿名内部类的实现
-
-
-
-        Button bn = (Button)findViewById(R.id.bn);
-        bn.setOnClickListener(new MyclickListener());
+        //内部类
+        Button bn1 = (Button) findViewById(R.id.bn);
+        bn1.setOnClickListener(new MyClickListener1());
 
         out = (Button)findViewById(R.id.outside);
         out.setOnClickListener(new View.OnClickListener() {
@@ -43,20 +33,41 @@ public class mainAcitvity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        //跳转到外部类的实现界面
+//跳转到外部类的实现界面
+
+
+        //匿名内部类
+        bn3 = (Button) findViewById(R.id.nm);
+        //匿名内部类
+        bn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                show.setText("匿名内部类");
+            }
+        });
+
+        //Activity自身作为事件监听器
+        show = (EditText) findViewById(R.id.txt);
+        bn4 = (Button) findViewById(R.id.act);
+        bn4.setOnClickListener(this);
+    }
+    //绑定到标签
+    public void clickHandler(View source){
+        EditText  show1 = (EditText) findViewById(R.id.txt);
+        show1.setText("绑定到标签");
     }
 
-
-
-
-
-    class MyclickListener implements View.OnClickListener{
+    //Activity
+    @Override
+    public void onClick(View v){
+        show.setText("Activity");
+    }
+    //内部类
+    class  MyClickListener1 implements View.OnClickListener{
         @Override
         public void onClick(View v){
-            EditText txt = (EditText) findViewById(R.id.txt);
-            txt.setText("内部类的实现");
+            EditText txt = (EditText) findViewById (R.id.txt);
+            txt.setText("内部类");
         }
     }
-
-
 }
